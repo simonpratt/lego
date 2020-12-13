0d7a1e0c0d2ba96ab47035257da79cd3e094b9d8
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2020-12-13 20:39:34 +1030

    Added verification screen

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,10 @@ export { default as Input } from './components/Input/Input.component';
 
 export { default as LoginScreen } from './screens/Login/Login.screen';
 export { default as RegisterScreen } from './screens/Register/Register.screen';
+export { default as VerificationScreen } from './screens/Verification/Verification.screen';
 
 export { default as Themes } from './theme/themes';
 
 export { RegisterData } from './screens/Register/Register.screen';
 export { LoginData } from './screens/Login/Login.screen';
+export { VerificationData } from './screens/Verification/Verification.screen';
