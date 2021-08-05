d160a72b409d60263f9a1431350111cbcc60a0c2
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2021-08-05 20:12:46 +0930

    Reduced card group padding

--- src/components/Card/CardGroup.component.tsx
+++ src/components/Card/CardGroup.component.tsx
@@ -5,6 +5,20 @@ export interface CardGroupProps {
   children: React.ReactNode;
 }
 
+const GroupSmall = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > * {
+    margin-bottom: 8px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`;
+
 const Group = styled.div`
   display: flex;
   flex-direction: column;
@@ -20,5 +34,7 @@ const Group = styled.div`
 `;
 
 const CardGroup = ({ children }: CardGroupProps) => <Group>{children}</Group>;
+
+CardGroup.Small = GroupSmall;
 
 export default CardGroup;
