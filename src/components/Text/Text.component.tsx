dab2464b6efda3f8fac5fc46074ea9c67201d1e0
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2021-08-22 10:58:50 +0930

    Added secondary text

--- src/components/Text/Text.component.tsx
+++ src/components/Text/Text.component.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+export type TextVariant = 'primary' | 'secondary';
+
 const TextContainer = styled.span`
   font-family: ${(props) => props.theme.fonts.default.family};
   font-size: ${(props) => props.theme.fonts.default.size};
@@ -9,10 +11,27 @@ const TextContainer = styled.span`
   color: ${(props) => props.theme.colours.defaultFont};
 `;
 
+const TextContainerSecondary = styled.span`
+  font-family: ${(props) => props.theme.fonts.default.family};
+  font-size: ${(props) => props.theme.fonts.default.size};
+  font-weight: ${(props) => props.theme.fonts.default.weight};
+
+  color: ${(props) => props.theme.colours.secondaryFont};
+`;
+
 export interface TextProps {
   children: React.ReactNode;
+  variant?: TextVariant;
 }
 
-const Text = ({ children }: TextProps) => <TextContainer>{children}</TextContainer>;
+const Text = ({ children, variant = 'primary' }: TextProps) => {
+  switch (variant) {
+    case 'secondary':
+      return <TextContainerSecondary>{children}</TextContainerSecondary>;
+    case 'primary':
+    default:
+      return <TextContainer>{children}</TextContainer>;
+  }
+};
 
 export default Text;
---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+import { Text } from '../..';
+
+export const Standard = () => <Text>test 1</Text>;
+
+export const Secondary = () => (
+  <>
+    <Text>This is not a drill</Text> <Text variant='secondary'>(no really!)</Text>
+  </>
+);
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+} as Meta;
