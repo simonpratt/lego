bd48f862e4bb6a6f9ffc501565a914ede50bc18a
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2021-08-22 12:15:02 +0930

    Fixed build

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -60,7 +60,7 @@ export const CardHeader = () => (
         magna aliqua.
       </Card.Content>
       <Card.Content>
-      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </Card.Content>
     </Card>
   </CardGroup>
