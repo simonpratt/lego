155a7ebba6aa16489edcc7c84d12a38128647e5f
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2021-12-28 09:27:21 +1030

    Added actions to page header

--- src/components/PageHeader/PageHeader.component.tsx
+++ src/components/PageHeader/PageHeader.component.tsx
@@ -3,18 +3,25 @@ import styled from 'styled-components';
 import { Heading } from '../..';
 
 const HeaderOuter = styled.div`
-  width: 100%;
-  padding: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: calc(100% - 64px);
+  margin: 32px;
+  min-height: 48px;
 `;
 
 export interface PageHeaderProps {
   heading: string;
+  actions?: React.ReactNode;
 }
 
-const PageHeader = ({ heading }: PageHeaderProps) => {
+const PageHeader = ({ heading, actions }: PageHeaderProps) => {
   return (
     <HeaderOuter>
       <Heading>{heading}</Heading>
+      <div>{actions}</div>
     </HeaderOuter>
   );
 };
--- src/components/PageHeader/PageHeader.stories.tsx
+++ src/components/PageHeader/PageHeader.stories.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
-import { PageHeader } from '../../';
+import { Button, ButtonGroup, PageHeader } from '../../';
 
 export const Standard = () => {
   return <PageHeader heading='Frank the Drummer' />;
+};
+
+export const WithActions = () => {
+  return (
+    <PageHeader
+      heading='Frank the Drummer'
+      actions={
+        <ButtonGroup>
+          <Button variant='tertiary'>Add</Button>
+        </ButtonGroup>
+      }
+    />
+  );
 };
 
 export default {
