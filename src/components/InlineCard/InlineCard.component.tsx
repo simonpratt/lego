18e46f66ef9baa738b01f1a1e17f98090684d8b8
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-01-28 22:50:03 +1030

    Added inline card gesture left

--- src/components/InlineCard/InlineCard.component.tsx
+++ src/components/InlineCard/InlineCard.component.tsx
@@ -1,26 +1,29 @@
+import { motion, PanInfo, useMotionValue, useTransform } from 'framer-motion';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
 import { responsive } from '../..';
+import getThemeStatusColour from '../../theme/helpers/getThemeStatusColour';
+import { Status } from '../../theme/theme.types';
 import InlineCardContent from './_InlineCardContent.component';
 import InlineCardMedia from './_InlineCardMedia.component';
 import InlineCardMeta from './_InlineCardMeta.component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export type InlineCardSize = 'fill' | 'xs' | 'sm' | 'md' | 'lg';
+export type DragVariant = Status;
 
-interface CardOuterProps {
-  usePointer: boolean;
+interface CardWrapperProps {
   size?: InlineCardSize;
 }
+interface CardOuterProps {
+  usePointer: boolean;
+}
 
-const CardOuter = styled.div<CardOuterProps>`
+const CardWrapper = styled.div<CardWrapperProps>`
+  position: relative;
   height: 64px;
-  width: 100%;
-  display: flex;
-
-  cursor: ${(props) => (props.usePointer ? 'pointer' : '')};
-
-  background-color: ${(props) => props.theme.colours.cardBackground};
-  box-shadow: ${(props) => props.theme.shadows.small};
 
   ${(props) => {
     switch (props.size) {
@@ -61,16 +64,82 @@ const CardOuter = styled.div<CardOuterProps>`
   }}
 `;
 
+const CardOuter = styled(motion.div)<CardOuterProps>`
+  height: 64px;
+  width: 100%;
+  display: flex;
+
+  cursor: ${(props) => (props.usePointer ? 'pointer' : '')};
+
+  background-color: ${(props) => props.theme.colours.cardBackground};
+  box-shadow: ${(props) => props.theme.shadows.small};
+`;
+
+const CardActionBackground = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 8px;
+  padding-right: 16px;
+
+  top: 0;
+  left: 0;
+  pointer-events: none;
+`;
+
 export interface InlineCardProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   size?: InlineCardSize;
+
+  gestureLeftIcon?: IconProp;
+  gestureLeftVariant?: DragVariant;
+  onGestureLeft?: () => void;
 }
 
-const InlineCard = ({ children, size, onClick }: InlineCardProps) => {
+const InlineCard = ({
+  children,
+  size,
+  onClick,
+  gestureLeftIcon,
+  gestureLeftVariant,
+  onGestureLeft,
+}: InlineCardProps) => {
+  const theme = useTheme();
+  const x = useMotionValue(0);
+  const xInput = [-70, 0];
+  const gestureLeftTheme = getThemeStatusColour(gestureLeftVariant || 'info', theme);
+
+  const opacity = useTransform(x, xInput, ['1', '0']);
+
+  const handleDragEnd = (e: MouseEvent | TouchEvent | PointerEvent, panInfo: PanInfo) => {
+    // approx xInputMin * 3
+    if (panInfo.offset.x < -180 && onGestureLeft) {
+      onGestureLeft();
+    }
+  };
+
   return (
-    <CardOuter size={size} usePointer={!!onClick} onClick={onClick}>
-      {children}
-    </CardOuter>
+    <CardWrapper size={size}>
+      <CardActionBackground style={{ opacity, backgroundColor: gestureLeftTheme.main }}>
+        {gestureLeftIcon && (
+          <FontAwesomeIcon style={{ fontSize: '20px', color: gestureLeftTheme.contrast }} icon={gestureLeftIcon} />
+        )}
+      </CardActionBackground>
+      <CardOuter
+        drag={onGestureLeft ? 'x' : undefined}
+        onDragEnd={handleDragEnd}
+        style={{ x }}
+        dragConstraints={{ left: 0, right: 0 }}
+        usePointer={!!onClick}
+        onClick={onClick}
+      >
+        {children}
+      </CardOuter>
+    </CardWrapper>
   );
 };
 
--- src/components/InlineCard/InlineCard.stories.tsx
+++ src/components/InlineCard/InlineCard.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
+import { faCheckCircle, faExclamationTriangle, faInfoCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Badge, InlineCard, InlineCardGroup } from '../..';
 
 export const Standard = () => (
@@ -25,6 +26,41 @@ export const Standard = () => (
       </InlineCard.Meta>
     </InlineCard>
   </InlineCardGroup>
+);
+
+export const WithDrag = () => (
+  <div style={{ maxWidth: '400px' }}>
+    <InlineCardGroup>
+      <InlineCard
+        onGestureLeft={() => console.log('Gesture left triggered')}
+        gestureLeftIcon={faCheckCircle}
+        gestureLeftVariant='success'
+      >
+        <InlineCard.Content>A success gesture</InlineCard.Content>
+      </InlineCard>
+      <InlineCard
+        onGestureLeft={() => console.log('Gesture left triggered')}
+        gestureLeftIcon={faInfoCircle}
+        gestureLeftVariant='info'
+      >
+        <InlineCard.Content>A info gesture</InlineCard.Content>
+      </InlineCard>
+      <InlineCard
+        onGestureLeft={() => console.log('Gesture left triggered')}
+        gestureLeftIcon={faExclamationTriangle}
+        gestureLeftVariant='warn'
+      >
+        <InlineCard.Content>A warning gesture</InlineCard.Content>
+      </InlineCard>
+      <InlineCard
+        onGestureLeft={() => console.log('Gesture left triggered')}
+        gestureLeftIcon={faTimes}
+        gestureLeftVariant='danger'
+      >
+        <InlineCard.Content>A danger gesture</InlineCard.Content>
+      </InlineCard>
+    </InlineCardGroup>
+  </div>
 );
 
 export default {
