2eb3a082626a9f52a6f483101d98549f85caf113
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-03-20 10:29:47 +1030

    Added click event for table action

--- src/components/Table/_TableAction.tsx
+++ src/components/Table/_TableAction.tsx
@@ -3,10 +3,15 @@ import Button from '../Button/Button.component';
 
 export interface TableActionProps {
   text: string;
+  onClick: () => void;
 }
 
-const TableAction = ({ text }: TableActionProps) => {
-  return <Button variant='tertiary'>{text}</Button>;
+const TableAction = ({ text, onClick }: TableActionProps) => {
+  return (
+    <Button variant='tertiary' onClick={onClick}>
+      {text}
+    </Button>
+  );
 };
 
 export default TableAction;
