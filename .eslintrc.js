6ff2bf7dd8b15af7eb294c0f9832df7b26992879
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-03-28 21:58:35 +1030

    Bumped eslint version

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,11 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:react/recommended',
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
   settings: {
