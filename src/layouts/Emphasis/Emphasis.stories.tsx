9776b1336becb3e85b86054a7224f1b602402a8b
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2022-09-14 12:33:08 +0930

    Upgraded more packages

---
+++ src/layouts/Emphasis/Emphasis.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+import { EmphasisLayout, Spacer, Input, Heading, Text } from '../..';
+
+export const Standard = () => (
+  <EmphasisLayout>
+    <Heading>Lets get setup..</Heading>
+    <Spacer size='4x' />
+    <Text>Enter your name here. This will be used to identify you when connecting with friends.</Text>
+    <Spacer size='2x' />
+    <Input name='name' placeholder='name'></Input>
+  </EmphasisLayout>
+);
+
+export default {
+  title: 'Layouts/Emphasis',
+  component: EmphasisLayout,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as Meta;
