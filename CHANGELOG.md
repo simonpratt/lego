9d2be0f6ccccb70799578bc491c946a17c72e4b2
Simon Pratt <19920260+simonpratt@users.noreply.github.com>
2023-01-01 12:52:01 +1030

    Added hiddenMenu prop to menu content

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+### v1.1.0
+
+- Added `hiddenMenu` prop to _MinimalMenu.Page_ to support changing between menu visible and menu hidden state without triggering a re-render
--- src/components/MinimalMenu/_MinimalMenuPage.component.tsx
+++ src/components/MinimalMenu/_MinimalMenuPage.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import responsive from '../../responsive/responsive';
 
-const MinimalMenuPageContainer = styled.div`
+const MinimalMenuPageContainer = styled.div<{ hiddenMenu: boolean }>`
   ${responsive.useStylesFor('tablet').andLarger(`
     padding-left: 64px;
   `)}
@@ -10,14 +10,25 @@ const MinimalMenuPageContainer = styled.div`
   ${responsive.useStylesFor('mobile').andSmaller(`
     padding-bottom: 64px;
   `)}
+
+  ${(props) =>
+    props.hiddenMenu &&
+    `
+    padding-left: 0 !important;
+    padding-bottom: 0 !important;
+  `}
 `;
 
 export interface MinimalMenuPageProps {
   children: React.ReactNode;
+  /** Set the page to be in hidden menu mode.
+   * This is useful for rendering your app and preventing re-renders between a un-authenticated no-menu mode and the regular authenticated mode
+   **/
+  hiddenMenu?: boolean;
 }
 
-const MinimalMenuPage = ({ children }: MinimalMenuPageProps) => {
-  return <MinimalMenuPageContainer>{children}</MinimalMenuPageContainer>;
+const MinimalMenuPage = ({ children, hiddenMenu }: MinimalMenuPageProps) => {
+  return <MinimalMenuPageContainer hiddenMenu={!!hiddenMenu}>{children}</MinimalMenuPageContainer>;
 };
 
 export default MinimalMenuPage;
